d4b9c7a89eac754f8edf83bcd327b9c91fc04f0c
asterox <62700841+roxov@users.noreply.github.com>
2020-06-01 00:01:15 +0200

    création de la Map et des itérateurs

--- Project02Eclipse/src/com/hemebiotech/analytics/Main.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/Main.java
@@ -2,14 +2,15 @@ package com.hemebiotech.analytics;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.TreeMap;
 
 public class Main {
 
 	public static void main(String[] args) throws IOException {
 		
 		List<String> symptomsList = new ReadSymptomDataFromFile("Project02Eclipse/resources/symptoms.txt").GetSymptoms();
-				
-		System.out.println(symptomsList);
+		TreeMap<String,Integer> sortedSymptomsMap = new SymptomCounter().counter(symptomsList);		
+		System.out.println(sortedSymptomsMap);
 		
 	}
 
--- Project02Eclipse/src/com/hemebiotech/analytics/SymptomCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/SymptomCounter.java
@@ -1,15 +1,41 @@
 package com.hemebiotech.analytics;
 
+import java.util.Iterator;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeMap;
+
 public class SymptomCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
-
-	public void counter() {
-		int i = 0;
-
-		int headCount = 0; // counts headaches
-		while (line != null) {
+	
+	public TreeMap<String,Integer> counter(List<String> symptomsList) {
+		TreeMap<String,Integer> result = new TreeMap<String,Integer>();
+		//Créer symptomsList ?
+		Iterator<String> it = symptomsList.iterator();
+		
+		//Generate an iterator to go through the keys added to the map of symptoms.
+		Set<String> keysResult = result.keySet();
+		Iterator<String> itmap = keysResult.iterator();
+		
+		
+		while(it.hasNext()) {
+			
+			
+			
+			if (itmap.next()) {
+			// Créer un itérateur de Map : for ?
+				Integer newValue = result.get(it.next()) + 1;
+				result.put(it.next(),newValue);
+				
+			// Si iterateur de la liste ne croise jamais l'itérateur de la map (clés)
+			// add symptom de la liste dans une nouvelle clé, avec une valeur de 1
+			} else {
+				result.put(it.next(), 1);
+			}
+		}
+		
+		return result;
+		
+		/*while (line != null) {
 			i++;
 			System.out.println("symptom from file: " + line);
 			if (line.equals("headache")) {
@@ -22,8 +48,7 @@ public class SymptomCounter {
 			}
 
 			line = reader.readLine(); // get another symptom
-		}
+		}*/
+		
 	}
-
 }
-
